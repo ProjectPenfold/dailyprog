2c7e323819625836ec01509d7d6683999395cc62
ProjectPenfold <alex.j.chadwick@gmail.com>
2018-11-25 15:29:41 +0000

    Revert "Add word list for Optional bonus 1"
    This reverts commit ffd4cfca4bb8d61b5756d7624cbc995c74b1ee1c.

---
+++ 366/366.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// def type for nodes in linked list
+typedef struct node 
+{
+    // change hard coded len of array (longest real word = 46 char)
+    char string[46];
+    struct node *next;
+}
+node;
+
+// create Word Funnel by removing single char at each postion of input str. Store in linked list
+int writeFunnel(char *input, node *first)
+{
+    // assign initial node value
+    strcpy(first->string, input);
+    first->next = NULL;
+
+    // create result on heap
+    char *result = malloc(sizeof(char) * (strlen(input)));
+
+    for (int inIndex = 0; inIndex < strlen(input); inIndex++)
+    {
+        // create copy of input and remove index char using memmove
+        strcpy(result, input);
+        memmove(&result[inIndex], &result[inIndex + 1], strlen(result) - inIndex);
+        
+        // assign new str to a new node in the linked list
+        node *new_node = malloc(sizeof(node));
+        if (!new_node) return 2;
+        strcpy(new_node->string, result);
+        new_node->next = NULL;
+
+        // assign node->next pointer values
+        if (!first->next)
+        {
+            first->next = new_node;
+        }
+        else
+        {
+            new_node->next = first->next;
+            first->next = new_node;
+        }
+    }
+}
+
+// search linked list from funnel()
+bool readFunnel(char *searchTerm, node *node1)
+{
+    // set LL cursor start point
+    node *cursor = node1;
+
+    // traverse linked list comparing strings
+    while (cursor != NULL)
+    {
+        if (strcmp(cursor->string, searchTerm) == 0) return true;
+        else cursor = cursor->next;
+    }
+
+    return false;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: funnel Src Test");
+        return 1;
+    }
+
+    char *original = argv[1];
+    char *test = argv[2];
+
+    // new linked list
+    node *node1 = malloc(sizeof(node));
+
+    // create linked list of possible new strings
+    writeFunnel(original, node1);
+
+    // search LL for input2
+    bool found = readFunnel(test, node1);
+
+    // display result
+    printf("Found: %s\n", found ? "true" : "false");
+
+    // free memory from var stored in heap
+    free(node1);
+}
+
