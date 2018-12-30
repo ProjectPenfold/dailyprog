9e9829ce928c401d84f5ee11173598169eb19a28
Alex Chadwick <alex.j.chadwick@gmail.com>
2018-12-30 19:53:39 +0000

    initial 364

---
+++ 364/364.c
@@ -0,0 +1,9 @@
+// [2018-06-18] Challenge #364 [Easy] Create a Dice Roller https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+    /* code */
+    return 0;
+}
