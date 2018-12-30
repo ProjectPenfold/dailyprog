8545699a6d6f5a1161065e0a40a77ed628eacf79
Alex Chadwick <alex.j.chadwick@gmail.com>
2018-12-30 19:08:48 +0000

    convert to upc() function for testing

--- 370/370.c
+++ 370/370.c
@@ -3,35 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+size_t upc(size_t input) {
+
     size_t evenSum = 0;
     size_t oddSum = 0;
     size_t checkDigit;
     char *paddedInput = malloc(11);
-
-    // Validate input
-    if (argc != 2) {
-        printf("More than one parameter in input. Usage: upc(<Up to 11 digits>)\n");
-        return 1;
-    }
-    // necessary because of hardcoded malloc limit in var paddedInput
-    if (strlen(argv[1]) > 11) {
-        printf("More than 11 digits in input. Usage: upc(<Up to 11 digits>)\n");
-        return 2;
-    }
-    
-    size_t input = atoi(argv[1]);
 
     // Add left hand side index padding, ensures total no. digits = 11
     sprintf(paddedInput, "%011zu", input);
 
     // Loop over each char in string input
     for (size_t i = 0; i < strlen(paddedInput); i++) {
-        // Check if even number
+        // Convert ASCII value of char to int. 
         size_t upcDigit = paddedInput[i] - 48;
         if (i % 2 == 0) {
-            // Append to existing even digit sum
             evenSum += upcDigit;
         } else {
             oddSum += upcDigit;
@@ -46,6 +32,16 @@ int main(int argc, char *argv[])
         checkDigit = 10 - M;
     }
 
-    printf("%s => %zu\n", paddedInput, checkDigit);
+    free(paddedInput);
+    return checkDigit;
+}
+
+int main(void) {
+
+    printf("upc(4210000526) => %zu\n", upc(4210000526));
+    printf("upc(3600029145) => %zu\n", upc(3600029145));
+    printf("upc(12345678910) => %zu\n", upc(12345678910));
+    printf("upc(1234567) => %zu\n", upc(1234567));
+
     return 0;
 }
